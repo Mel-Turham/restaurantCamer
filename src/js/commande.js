c5a2442e4904f73c77352f7bbd0a6d9da5a59238
Mel Turham <tchoikuemel06@gmail.com>
2024-01-14 15:27:57 +0100

    update

--- src/js/commande.js
+++ src/js/commande.js
@@ -24,21 +24,18 @@ buttonOpenNav.addEventListener('click', openNav);
 
 let total = document.querySelector('.setTotal');
 
-const convertTotal = Number(total.textContent);
-
 const inputNumber = document.getElementById('quantiter');
 let priceFinal = document.querySelector('.final-price');
+const convertTotal = Number(total.textContent);
 
 inputNumber.addEventListener('input', () => {
 	if (inputNumber.value <= 0) {
 		alert('La quantiter ne doit pas etre negatif ou inferieur a 0');
 	} else {
 		total.textContent = convertTotal * inputNumber.value;
-    priceFinal.textContent = total.textContent;
+		priceFinal.textContent = total.textContent;
 	}
 });
-
-
 
 const buttonPay = document.querySelector('.js-button-pay');
 const overley = document.querySelector('.overley');
@@ -46,16 +43,52 @@ const closeModalForm = document.querySelector('.js-close-modal-form');
 
 buttonPay.addEventListener('click', () => {
 	overley.classList.add('display');
+	boxModal.classList.toggle('active');
 });
 
 closeModalForm.addEventListener('click', () => {
 	overley.classList.remove('display');
 });
 
+const fieldInputs = document.querySelector('.field-inputs');
+const inputs = fieldInputs.querySelectorAll('input');
+const selectOption = fieldInputs.querySelector('select');
+
+const buttonSubmit = document.querySelector('.js-button-submit');
+
+console.log(buttonSubmit);
+
+inputs.forEach((input) => {
+	if (input.value === '') {
+		buttonSubmit.setAttribute('disabled', true);
+	} else {
+		buttonSubmit.removeAttribute('disabled');
+	}
+});
+
+selectOption.addEventListener('change', () => {
+	if (selectOption.value === '') {
+		buttonSubmit.setAttribute('disabled', true);
+	} else {
+		buttonSubmit.removeAttribute('disabled');
+	}
+});
 
 const formPay = document.getElementById('form-payemant');
 
-formPay.addEventListener('submit', (event)=>{
-  event.preventDefault();
- 
+const boxModal = document.querySelector('.modal-form');
+
+const messageBox = document.querySelector('.remerciment');
+
+formPay.addEventListener('submit', (event) => {
+	event.preventDefault();
+	boxModal.style.display = 'none';
+	messageBox.style.display = 'flex';
+
+	const closeAll = document.querySelector('.js-close-all-modal');
+
+	closeAll.addEventListener('click', () => {
+		messageBox.style.display = 'none';
+		overley.classList.remove('display');
+	});
 });
